08fbc9d8e25bc2fe1ee4203267438193511108c0
Shiv Jindal <shivjindal.dda@gmail.com>
2025-05-26 09:49:30 +0530

    initial html

---
+++ script.js
@@ -0,0 +1,18 @@
+function add() {
+
+
+}
+
+
+function remove() {
+
+
+}
+
+    let count = 0;
+
+function changeText() {
+
+    document.getElementById("header").innerText = `Button is clicked ` + (++count) + ` times` ;
+
+}
